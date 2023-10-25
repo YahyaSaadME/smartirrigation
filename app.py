2e69d71a8696d8e591822ff0a982f5fec03558af
Yahya saad <yahyasaad.me@gmail.com>
2023-10-25 15:12:36 +0530

    I

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+# Create a Flask application instance
+app = Flask(__name__)
+
+# Define a route and associated view function
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/temp')
+def hello_world():
+    return 10.34
+
+if __name__ == '__main__':
+    # Run the Flask application on a local development server
+    app.run()
